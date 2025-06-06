488c06af1b4dd32673d0eaf49c63fa1a78f4b92d
Theobl08 <theobl08@gmail.com>
2025-06-06 23:47:08 +0200

    Reintroduce catwalks at the bottom of the block

--- common/src/main/java/com/github/talrey/createdeco/blocks/CatwalkRailingBlock.java
+++ common/src/main/java/com/github/talrey/createdeco/blocks/CatwalkRailingBlock.java
@@ -1,6 +1,8 @@
 package com.github.talrey.createdeco.blocks;
 
 import com.simibubi.create.content.equipment.wrench.IWrenchable;
+import com.simibubi.create.api.schematic.requirement.SpecialBlockItemRequirement;
+import com.simibubi.create.content.schematics.requirement.ItemRequirement;
 import com.simibubi.create.foundation.block.ProperWaterloggedBlock;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -16,6 +18,7 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
@@ -30,7 +33,7 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 
 import javax.annotation.Nullable;
 
-public class CatwalkRailingBlock extends Block implements IWrenchable, ProperWaterloggedBlock {
+public class CatwalkRailingBlock extends Block implements IWrenchable, ProperWaterloggedBlock, SpecialBlockItemRequirement {
   private static final VoxelShape VOXEL_NORTH = Block.box(
           0d, 0d, 0d,
           16d, 14d, 2d
@@ -53,8 +56,11 @@ public class CatwalkRailingBlock extends Block implements IWrenchable, ProperWat
   public static final BooleanProperty EAST_FENCE  = BlockStateProperties.EAST;
   public static final BooleanProperty WEST_FENCE  = BlockStateProperties.WEST;
 
-  public CatwalkRailingBlock (Properties props) {
+  public final String metal;
+
+  public CatwalkRailingBlock (Properties props, String metal) {
     super(props);
+    this.metal = metal;
     this.registerDefaultState(this.defaultBlockState()
             .setValue(NORTH_FENCE, false)
             .setValue(SOUTH_FENCE, false)
@@ -254,5 +260,19 @@ public class CatwalkRailingBlock extends Block implements IWrenchable, ProperWat
     }
     BlockState newState = defaultBlockState().setValue(NORTH_FENCE, north).setValue(SOUTH_FENCE, south).setValue(EAST_FENCE, east).setValue(WEST_FENCE, west);
     return newState;
+  }
+
+  @Override
+  public ItemRequirement getRequiredItems(BlockState state, BlockEntity blockEntity) {
+    int count = 0;
+    count += state.getValue(NORTH_FENCE)? 1 : 0;
+    count += state.getValue(EAST_FENCE)? 1 : 0;
+    count += state.getValue(SOUTH_FENCE)? 1 : 0;
+    count += state.getValue(WEST_FENCE)? 1 : 0;
+
+    return new ItemRequirement(
+      ItemRequirement.ItemUseType.CONSUME,
+      new ItemStack(this.asItem(), count)
+    );
   }
 }
